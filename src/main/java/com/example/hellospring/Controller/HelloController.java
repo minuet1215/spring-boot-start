f98df6c0fc39e88b0a3dfe7d1f6b1e70d1743a3f
jungle <minuet1215@jbnu.ac.kr>
2022-11-24 03:22:59 +0900

    Feat(HelloController, hello.html): Controller와 thymeleaf template engine을 통한 동적 페이지 생성

--- src/main/java/com/example/hellospring/Controller/HelloController.java
+++ src/main/java/com/example/hellospring/Controller/HelloController.java
@@ -1,2 +1,15 @@
-package com.example.hellospring.Controller;public class HelloController {
+package com.example.hellospring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
 }
