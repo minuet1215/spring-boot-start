ab66e5b69c88e52be887e57fdb827bdede36481b
jungle <minuet1215@jbnu.ac.kr>
2022-11-27 17:48:29 +0900

    Test: 회원 중복 예외 테스트 추가

--- src/main/java/com/example/hellospring/service/MemberService.java
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package com.example.hellospring.service;
 
 import com.example.hellospring.domain.Member;
 import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member) {
         validateDuplicateMember(member);
@@ -21,7 +24,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalArgumentException("이미 졵애하는 회원입니다.");
+                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
                 });
     }
 
--- src/test/java/com/example/hellospring/service/MemberServiceTest.java
+++ src/test/java/com/example/hellospring/service/MemberServiceTest.java
@@ -1,29 +1,66 @@
 package com.example.hellospring.service;
 
 import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
 
     @DisplayName("회원가입 테스트")
     @Test
     void join() {
         // given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
 
         // when
         Long saveId = memberService.join(member);
 
         // then
         Member findMember = memberService.findOne(saveId).get();
-        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @DisplayName("중복 회원 예외 테스트")
+    @Test
+    public void duplicatedMemberJoin() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+
+        assertThatThrownBy(() -> memberService.join(member2)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이미 존재하는 회원입니다.");
+        // then
+
+
     }
 
     @DisplayName("회원 전체 조회 테스트")
