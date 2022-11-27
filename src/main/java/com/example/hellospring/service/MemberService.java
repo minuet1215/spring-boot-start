3684d422a3512f9d0c4c1a249d17c57c5191c6c7
jungle <minuet1215@jbnu.ac.kr>
2022-11-27 17:25:49 +0900

    Feat(MemberService): 회원 가입, 회원 전체 조회, 회원 개별 조회 구현

---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 졵애하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
